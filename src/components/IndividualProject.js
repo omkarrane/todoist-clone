89f96b1d935e1fea151bd57230cb89864b011829
Omkar Rane <omkarrane10@gmail.com>
2020-05-28 03:47:05 +0530

    Completed IndividualProject component

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -19,4 +19,30 @@ export const IndividualProject = ({ project }) => {
         setSelectedProject('INBOX');
       });
   };
+
+  return (
+    <>
+      <span className="sidebar__dot">.</span>
+      <span className="sidebar__project-name">{project.name}</span>
+      <span
+        className="sidebar__project-delete"
+        data-testid="delete-project"
+        onClick={() => setShowConfirm(!showConfirm)}
+      >
+        <FaTrashAlt />
+        {showConfirm && (
+          <div className="project-delete-modal">
+            <div className="project-delete-modal__inner">
+              <p>Are you sure you want to delete this project?</p>
+              <button type="button" onClick={() => deleteProject(project.docId)}
+              >
+                Delete
+                 <span onClick={() => setShowConfirm(!showConfirm)}>Cancel</span>
+              </button>
+            </div>
+          </div>
+        )}
+      </span>
+    </>
+  );
 }; 
